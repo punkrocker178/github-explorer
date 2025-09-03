24e81dec230c672546d51c7fb9997875cfc2e4de
Hieu Le <ngochieu.le1708@gmail.com>
2025-09-03 12:49:06 +0700

    Update script

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 pm2 delete 0
-pm2 start server.js
+pm2 start "$SCRIPT_DIR/server.js"
