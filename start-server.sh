7312f47b0060615d7f27233ed38661e886336890
Hieu Le <ngochieu.le1708@gmail.com>
2025-09-03 12:59:30 +0700

    Use node to start server instead of pm2

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,7 @@
 # Get the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pm2 delete 0
-pm2 start "$SCRIPT_DIR/server.js"
+#pm2 delete 0
+#pm2 start "$SCRIPT_DIR/server.js"
+cd "$SCRIPT_DIR"
+nohup node server.js > /tmp/server.log 2>&1 &
