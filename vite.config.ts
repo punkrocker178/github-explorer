a4fc04cec16075262ed5b82b4aa5649003d5f2c0
Hieu Le <ngochieu.le1708@gmail.com>
2025-09-22 22:52:00 +0700

    Update readme, vite proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://gh-server:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
